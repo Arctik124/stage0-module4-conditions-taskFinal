6bd44f4802dde68ccc446b5fb47973b189d1f674
Artyom <artvit12@gmail.com>
2022-08-11 20:51:47 +0600

    Solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/SeasonDeterminer.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class SeasonDeterminer {
     public void tellTheSeason(int monthNumber) {
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Wrong month number");
+        } else if (monthNumber < 3 || monthNumber == 12) {
+            System.out.println("Winter");
+        } else if (monthNumber < 6) {
+            System.out.println("Spring");
+        } else if (monthNumber < 9) {
+            System.out.println("Summer");
+        } else {
+            System.out.println("Autumn");
+        }
     }
 }
